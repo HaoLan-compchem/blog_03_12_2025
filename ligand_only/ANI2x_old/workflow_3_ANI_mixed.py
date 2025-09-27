7a264d12a07030708e6e5b01638a5dd221e4adba
HaoLan-compchem <cha13hl@gmail.com>
2025-09-27 14:56:37 +0100

    openmm-ml bug

---
+++ ligand_only/ANI2x_old/workflow_3_ANI_mixed.py
@@ -0,0 +1,122 @@
+import sys
+import torch
+import numpy as np
+#
+import openmm as mm
+import openmm.unit as unit
+import openmm.app as app
+from openmm.app import PDBFile
+#
+from openmmtorch import TorchForce
+from torchani.models import ANI2x
+
+#-----------------------------------------------------------------------------------------------------------------#
+
+cuda_available = torch.cuda.is_available()
+print(f"CUDA Available: {cuda_available}")
+
+if cuda_available:
+    num_gpus = torch.cuda.device_count()
+    print(f"Number of GPUs: {num_gpus}")
+    current_gpu_name = torch.cuda.get_device_name(0)
+    print(f"Current GPU Name: {current_gpu_name}")
+    print("\nGPU Memory Usage:")
+    print(f"  Allocated: {round(torch.cuda.memory_allocated(0) / 1024**3, 1)} GB")
+    print(f"  Cached: {round(torch.cuda.memory_reserved(0) / 1024**3, 1)} GB")
+else:
+    print("PyTorch is running on CPU.")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"\nUsing device: {device}")
+
+#-----------------------------------------------------------------------------------------------------------------#
+
+with open('gaff_system.xml', 'r') as f:
+    system = mm.XmlSerializer.deserialize(f.read())
+pdb = PDBFile('gaff_ligand_in_solvent.pdb')
+#
+chains = list(pdb.topology.chains())
+print(chains)
+ml_atoms = [atom.index for atom in chains[0].atoms()]
+print(ml_atoms)
+atomic_numbers = [atom.element.atomic_number for atom in chains[0].atoms()]
+print(atomic_numbers)
+
+#-----------------------------------------------------------------------------------------------------------------#
+
+from workshop_utils import removeBonds
+
+def removeMMInteraction(system, ml_atoms):
+    newSystem = removeBonds(system, ml_atoms)
+    for force in newSystem.getForces():
+        if isinstance(force, mm.NonbondedForce):
+            for i in range(len(ml_atoms)):
+                for j in range(i):
+                    force.addException(ml_atoms[i], ml_atoms[j], 0, 1, 0, True)
+        elif isinstance(force, mm.CustomNonbondedForce):
+            existing = set(tuple(force.getExclusionParticles(i)) for i in range(force.getNumExclusions()))
+            for i in range(len(ml_atoms)):
+                a1 = ml_atoms[i]
+                for j in range(i):
+                    a2 = ml_atoms[j]
+                    if (a1, a2) not in existing and (a2, a1) not in existing:
+                        force.addExclusion(a1, a2, True)
+    return newSystem
+
+mixed_system = removeMMInteraction(system, ml_atoms)
+print("number of forces (before nnp)  = ", mixed_system.getNumForces())
+
+#-----------------------------------------------------------------------------------------------------------------#
+
+class HybridNNP(torch.nn.Module):
+    def __init__(self, atomic_numbers, ml_atoms):
+        super().__init__()
+        self.indices = torch.tensor(ml_atoms, dtype=torch.long, device=device)
+        self.atomic_numbers = torch.tensor(atomic_numbers, device=device).unsqueeze(0)
+        self.model = ANI2x(periodic_table_index=True)
+        self.model.to(self.atomic_numbers.device)
+    def forward(self, positions):
+        positions = positions.to(self.atomic_numbers.device)
+        positions = positions[self.indices]
+        positions = positions.unsqueeze(0).float() * 10 # nm -> Å
+        result = self.model((self.atomic_numbers, positions))
+        energy = result.energies[0] * 2625.5 # Hartree -> kJ/mol
+        return energy
+
+#------------------------------------------------------------------------------------------------#
+
+hybrid_nnp = HybridNNP(atomic_numbers, ml_atoms)
+torch_module = torch.jit.script(hybrid_nnp)
+torchforce = TorchForce(torch_module)
+mixed_system.addForce(torchforce)
+
+print("number of forces (after nnp) = ", mixed_system.getNumForces())
+
+#-------------------------------------------------------------------------------------------------#
+
+# Set up simulation
+temperature = 300 * unit.kelvin
+frictionCoeff = 1 / unit.picosecond
+timeStep = 0.5 * unit.femtosecond
+integrator = mm.LangevinMiddleIntegrator(temperature, frictionCoeff, timeStep)
+#
+simulation = app.Simulation(pdb.topology, mixed_system, integrator)
+print("Simulation is running on:", simulation.context.getPlatform().getName())
+#
+simulation.reporters.clear()
+simulation.context.setPositions(pdb.positions)
+simulation.minimizeEnergy()
+with open(f'min.pdb', 'w') as f:
+    app.PDBFile.writeFile(simulation.topology, simulation.context.getState(getPositions=True).getPositions(), f)
+#
+state = simulation.context.getState(getEnergy=True, getForces=True)
+openmm_energy = state.getPotentialEnergy().value_in_unit(unit.kilojoule_per_mole)
+print(openmm_energy)
+openmm_force = state.getForces(asNumpy=True).value_in_unit(unit.kilojoule_per_mole/unit.nanometer)
+print(openmm_force)
+#
+simulation.reporters.append(app.DCDReporter('test_ani_mixed.dcd', 100))
+simulation.context.setVelocitiesToTemperature(temperature)
+reporter = app.StateDataReporter(file=sys.stdout, reportInterval=100, step=True, time=True, potentialEnergy=True, temperature=True, speed=True)
+simulation.reporters.append(reporter)
+simulation.step(10000)
--- ligand_only/classic_ANI2x/workflow.py
+++ ligand_only/classic_ANI2x/workflow.py
@@ -1,41 +1,58 @@
-import sys
-import torch
-import numpy as np
-#from torchani.models import ANI2x
+#import sys
+#import torch
+#import numpy as np
+#
 import openmm as mm
 import openmm.app as app
 import openmm.unit as unit
 from openmm.app import PDBFile
-#from openmmtorch import TorchForce
+from openmm import Platform
+#
 from openmmml import MLPotential # this module include functions of torch, torchani and openmmtorch
 #
-cuda_available = torch.cuda.is_available()
-print(f"CUDA Available: {cuda_available}")
+#cuda_available = torch.cuda.is_available()
+#print(f"CUDA Available: {cuda_available}")
 
-if cuda_available:
-    num_gpus = torch.cuda.device_count()
-    print(f"Number of GPUs: {num_gpus}")
-    current_gpu_name = torch.cuda.get_device_name(0)
-    print(f"Current GPU Name: {current_gpu_name}")
-    print("\nGPU Memory Usage:")
-    print(f"  Allocated: {round(torch.cuda.memory_allocated(0) / 1024**3, 1)} GB")
-    print(f"  Cached: {round(torch.cuda.memory_reserved(0) / 1024**3, 1)} GB")
-else:
-    print("PyTorch is running on CPU.")
+#if cuda_available:
+    #num_gpus = torch.cuda.device_count()
+    #print(f"Number of GPUs: {num_gpus}")
+    #current_gpu_name = torch.cuda.get_device_name(0)
+    #print(f"Current GPU Name: {current_gpu_name}")
+    #print("\nGPU Memory Usage:")
+    #print(f"  Allocated: {round(torch.cuda.memory_allocated(0) / 1024**3, 1)} GB")
+    #print(f"  Cached: {round(torch.cuda.memory_reserved(0) / 1024**3, 1)} GB")
+#else:
+    #print("PyTorch is running on CPU.")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"\nUsing device: {device}")
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cpu")
+#print(f"\nUsing device: {device}")
 #
-with open('gaff_system.xml', 'r') as f:
-    system_1 = mm.XmlSerializer.deserialize(f.read())
-pdb = PDBFile('gaff_ligand_in_solvent.pdb')
+#with open('gaff_system.xml', 'r') as f:
+    #system_1 = mm.XmlSerializer.deserialize(f.read())
+#pdb = PDBFile('gaff_ligand_in_solvent.pdb')
 #system_1.addForce(mm.MonteCarloBarostat(1*unit.atmosphere, 300*unit.kelvin))
 #
-chains = list(pdb.topology.chains())
-ml_atoms = [atom.index for atom in chains[0].atoms()]
+from openff.toolkit.topology import Molecule
+molecule=Molecule.from_smiles('CCCCC')
+molecule.generate_conformers(n_conformers=1)
+positions=molecule.conformers[0].to_openmm()
+from openmmforcefields.generators import GAFFTemplateGenerator
+gaff = GAFFTemplateGenerator(molecules=molecule)
+forcefield = app.ForceField('amber14-all.xml', 'amber14/tip3pfb.xml')
+forcefield.registerTemplateGenerator(gaff.generator)
 #
-potential = MLPotential('ani2x', implementation='nnpops')
-mixed_system = potential.createMixedSystem(pdb.topology, system_1, ml_atoms)
+modeller = app.Modeller(molecule.to_topology().to_openmm(), positions)
+modeller.addSolvent(forcefield, model='tip3p', padding=1.0*unit.nanometers)
+system_1 = forcefield.createSystem(modeller.topology, nonbondedMethod=app.PME, constraints=None)
+#
+chains = list(modeller.topology.chains())
+print(chains)
+ml_atoms = [atom.index for atom in chains[0].atoms()]
+print(ml_atoms)
+#
+potential = MLPotential('ani2x')
+mixed_system = potential.createMixedSystem(modeller.topology, system_1, ml_atoms)
 #
 # Create an integrator with a time step of 0.5 fs (ensure NN potential gradient force precision and system stability)
 temperature = 300 * unit.kelvin
@@ -44,9 +61,9 @@ timeStep = 0.5 * unit.femtosecond
 integrator = mm.LangevinMiddleIntegrator(temperature, frictionCoeff, timeStep)
 
 # Create a simulation and set the initial positions and velocities
-simulation = app.Simulation(pdb.topology, mixed_system, integrator)
+simulation = app.Simulation(modeller.topology, mixed_system, integrator)
 print("Simulation is running on:", simulation.context.getPlatform().getName())
-simulation.context.setPositions(pdb.positions)
+simulation.context.setPositions(modeller.positions)
 simulation.context.setVelocitiesToTemperature(temperature)
 
 # Minimize the system
